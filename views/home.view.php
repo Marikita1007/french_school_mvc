4f6e3c6609f16f3ab911c0947a58f8b01bc2c37d
Marikita1007 <ishinomaki1007@gmail.com>
2021-12-29 01:10:54 +0100

    Added a section in a page home

--- views/home.view.php
+++ views/home.view.php
@@ -73,10 +73,25 @@ ob_start();
 
 </section>
 
+<section id="instructions">
+    <div class="wave-container">
+        <div class="content">
+            <h3 class="text-center mb-5">Une méthode pour chaque type d'apprentissage</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <i class="far fa-check-circle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatum minima, earum maxime sequi tempora.</i>
+            <i class="far fa-check-circle">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, quisquam.</i>
+            <i class="far fa-check-circle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto explicabo aliquid consequatur quaerat.</i>
+        </div>
+        <img src="public/img/mobile-course.jpg" alt="Online Course">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00008B" fill-opacity="0.3" d="M0,32L60,69.3C120,107,240,181,360,208C480,235,600,213,720,176C840,139,960,85,1080,90.7C1200,96,1320,160,1380,192L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+    </div>
+    
+</section>
+
 <section id="temoignages">
     <div class="container">
         <div class="row introduction_titles">
-            <h2 class="">Témoignages</h2>
+            <h2 class="mt-5">Témoignages</h2>
         </div>
     </div>
 

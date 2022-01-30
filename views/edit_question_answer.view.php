2427c40c04681b23c8453a9be0e5b124dc0493c9
Marikita1007 <ishinomaki1007@gmail.com>
2022-01-30 13:03:11 +0100

    added underline animation and hover animation

--- views/edit_question_answer.view.php
+++ views/edit_question_answer.view.php
@@ -2,29 +2,6 @@
 ob_start();
 ?>
 
-    <section id="admin">
-    <nav class="navbar navbar-light bg-light p-3">
-        <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
-            <a class="navbar-brand" href="#">
-                Administration
-            </a>
-            <button class="navbar-toggler d-md-none collapsed mb-3" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="sidebar" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-
-        <div class="col-12 col-md-5 col-lg-8 d-flex align-items-center justify-content-md-end mt-3 mt-md-0">
-
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
-                    Bonjour, <?= $_SESSION['member']->nom ?>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item" href="logout.php">Se déconnecter</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
     <div class="container-fluid">
         <div class="row">
             <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
@@ -103,7 +80,7 @@ ob_start();
                                 </select>
 
                                 <input type="submit" value="Modifier la question et les réponses" class="btn btn-primary my-3">
-                                <a href="?view=question&opAdmin=qlist" >Return</a>
+                                <button class="btn"><a href="?view=question&opAdmin=qlist">Retourner</a></button>
 
                             </form>
                         </div>

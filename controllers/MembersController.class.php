71d4a1d369192d69d36c2dfa29cf249e28033653
Marikita1007 <ishinomaki1007@gmail.com>
2022-02-18 09:44:34 +0100

    Moved register to Sign controller

--- controllers/MembersController.class.php
+++ controllers/MembersController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace controllers;
 
+use Debug;
 use models\Model;
 use models\MembersManager;
 use models\QuestionsManager;
@@ -21,20 +22,15 @@ class MembersController
     }
 
     public function run(){
+        //このlist多分削除可能
         $op = $_GET['op'] ?? 'list';
 
         switch ($op){
-            case 'list' :
-                break;
-                $this->listAll();
             case 'editCheck':
                 $this->editCheck();
                 break;
             case 'edit' :
                 $this->edit();
-                break;
-            case 'editPsw':
-                $this->editPassword();
                 break;
             case 'show' :
                 $this->showData();
@@ -80,13 +76,13 @@ class MembersController
         header('location:?view=home');
     }
 
-    //!!!!何故か$_GET['id_member']が空
     public function deleteCheck(){
         require ('views/verify_delete.view.php');
     }
 
     public function delete(){
         if($_GET['op'] == 'delete' && !empty($_SESSION['member']->id_member) && is_numeric($_SESSION['member']->id_member)){
+            $this->db->deleteTestResult($_SESSION['member']->id_member);  
             $this->db->deleteData($_SESSION['member']->id_member);
         }
         require ('views/confirm_deleted.view.php');
@@ -127,7 +123,6 @@ class MembersController
                     //header('location:?view=home&op=admin');
                 }else{
                     //header('location:?view=home');
-                    //これなんでアドミンは何もしないのにでるのか。多分セッション。そのほうがきれい。
                     $testResultDatas = $this->dbtestResults->checkUserHistory($_SESSION["member"]->id_member);
                     if($testResultDatas){
                         $testResult = $testResultDatas->test_result;
@@ -172,28 +167,58 @@ class MembersController
 
     public function edit(){
 
-//        if($_GET['op'] == 'edit' && !empty($_GET['id_member']) && is_numeric($_GET['id_member'])){
-//            $memberInfo = $this->db->selectOne($_GET['id_member']);
-//        }
-
         if(!empty($_POST)){
 
             $errors = array();
             $champs_vides = 0;
+
+            foreach ($_POST as $key => $value) {
+                if (trim($_POST[$key]) == '') $champs_vides++;
+                // j'incrementxe mon compteur de champs vides chaque fois que je detecte un champ non rempli
+            }
+
+            if ($champs_vides > 0) {
+
+                $errors[] = '<div class="alert alert-danger">Il manque ' . $champs_vides . ' information(s).</div>';
+            }
+
+            if(!isset($_POST['nom']) || strlen($_POST['nom']) < 2 || strlen($_POST['nom']) > 30 ){
+                $errors[] = '<div class="alert alert-danger">Le prenom doit contenir entre 2 et 30 caractères. </div>';
+            }
+        
+            if(!isset($_POST['prenom']) || strlen($_POST['prenom']) < 2 || strlen($_POST['nom']) > 30 ){
+                $errors[] = '<div class="alert alert-danger">Le prenom doit contenir entre 2 et 30 caractères. </div>';
+            }
+        
+            if(!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL )){
+                $errors[] = '<div class="alert alert-danger"> Email non valide. </div>';
+            }
+        
+            if(!isset($_POST['password']) || strlen($_POST['password']) < 8 || strlen($_POST['password']) > 20){
+                $errors[] = '<div class="alert alert-danger"> Le mot de passe doit contenir entre 8 et 20 caractères. </div>';
+            }
+    
+            if (!preg_match("#[0-9]+#", $_POST['password'])) {
+                $errors[] = '<div class="alert alert-danger"> Le mot de passe doit inclure au moins un numéro. </div>';
+            }
+        
+            if (!preg_match("#[a-z]+#", $_POST['password'])) {
+                $errors[] = '<div class="alert alert-danger">Le mot de passe doit contenir au moins une lettre. </div>';
+            }  
+    
+            if (!preg_match("#[A-Z]+#", $_POST['password'])) {
+                $errors[] = '<div class="alert alert-danger">Le mot de passe doit contenir au moins une lettre capitale. </div>';
+            }
+
+            if(!isset($_POST['password']) || $_POST['password'] != $_POST['password2']){
+                $errors[] = '<div class="alert alert-danger">Le mot de passe ne correspond pas.</div>';
+            }
+
             foreach ($_POST as $key => $value) {
                 $_POST[$key] = htmlspecialchars($value);
                 if($key == "password" || $key == "password2"){
                     $_POST[$key] = md5($_POST[$key]);
                 }
-                if (trim($_POST[$key]) == '') $champs_vides++;
-                // j'incrementxe mon compteur de champs vides chaque fois que je detecte un champ non rempli
-            }
-            if ($champs_vides > 0) {
-                $errors[] = '<div class="alert alert-danger">Il manque ' . $champs_vides . ' information(s).</div>';
-            }
-
-            if(!isset($_POST['password']) || $_POST['password'] != $_POST['password2']){
-                $errors[] = '<div class="alert alert-danger">Le mot de passe ne correspond pas.</div>';
             }
 
             if (empty($errors)) {
@@ -203,68 +228,13 @@ class MembersController
                     $this->db->update($_SESSION['member']->id_member, $_POST);// je lance la méthode update du model
                 }
 
+            }else{
+                require('views/edit_member.view.php');
             }
         }
+
         $_SESSION['member'] = $this->db->selectOne($_SESSION['member']->id_member);
         require ('views/confirm_edited.view.php');
-
-    }
-
-    public function register(){
-
-        $errors = "";
-
-        if(!empty($_POST)){
-
-            $champs_vides = 0;
-            foreach ($_POST as $key => $value){
-                $_POST[$key] = htmlspecialchars($value);
-                if (trim($_POST[$key]) == '') $champs_vides++;
-            }
-            if($champs_vides > 0){
-                $errors .= '<div class="alert alert-danger">Il manque ' . $champs_vides . ' information(s).</div>';
-            }
-        }
-
-        if(!isset($_POST['nom']) || strlen($_POST['nom']) < 2 || strlen($_POST['nom']) > 30 ){
-            $errors .= '<div class="alert alert-danger">Le prenom doit contenir entre 2 et 30 caractères. </div>';
-        }
-
-        if(!isset($_POST['prenom']) || strlen($_POST['prenom']) < 2 || strlen($_POST['nom']) > 30 ){
-            $errors .= '<div class="alert alert-danger">Le prenom doit contenir entre 2 et 30 caractères. </div>';
-        }
-
-        if(!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL )){
-            $errors .= '<div class="alert alert-danger"> Email non valide. </div>';
-        }
-
-        if(!isset($_POST['password']) || strlen($_POST['password']) < 5 || strlen($_POST['password']) > 20){
-            $errors .= '<div class="alert alert-danger"> Le mot de passe doit contenir entre 5 et 20 caractères. </div>';
-        }
-
-        if(!isset($_POST['password']) || $_POST['password'] != $_POST['password2']){
-            $errors .= '<div class="alert alert-danger">Le mot de passe ne correspond pas.</div>';
-        }
-
-
-        if($errors == ""){
-            $showPage = "";
-            unset($_POST['password2']);
-            $_POST['password'] = md5($_POST['password']);//password encrypted
-            if($_GET['op'] == 'newMember'){
-                $newMemberId = $this->db->insert($_POST);
-                if($newMemberId > 0){
-                    $_SESSION['member'] = $this->db->selectOne($newMemberId);
-                    require('views/register_confirm.view.php');
-                }else{
-                    $errors .= '<div class="alert alert-danger" >Votre email est déjà utilisé.</div>';
-                    require('views/sign_up.view.php');
-                }
-            }
-        }else{
-            require('views/sign_up.view.php');
-        }
-
     }
 
     public function showTest(){
--- controllers/SignController.class.php
+++ controllers/SignController.class.php
@@ -2,19 +2,97 @@
 
 namespace controllers;
 
+use models\Model;
+use PDO, PDOException, Exception;
+use Debug;
+
 class SignController
 {
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new \Models\MembersManager();
+    }
+
     public function run(){
 
         $view = $_GET['op'] ?? 'register';
         switch ($view) {
             case 'newMember' :
-                $newMember = new MembersController;
-                $newMember->register();
+                $this->register();
                 break;
             default :
                 require('views/sign_up.view.php');
                 break;
         }
+    }
+
+    public function register(){
+
+        $errors = "";
+    
+        if(!empty($_POST)){
+    
+            $champs_vides = 0;
+            foreach ($_POST as $key => $value){
+                $_POST[$key] = htmlspecialchars($value);
+                if (trim($_POST[$key]) == '') $champs_vides++;
+            }
+            if($champs_vides > 0){
+                $errors .= '<div class="alert alert-danger">Il manque ' . $champs_vides . ' information(s).</div>';
+            }
+        }
+    
+        if(!isset($_POST['nom']) || strlen($_POST['nom']) < 2 || strlen($_POST['nom']) > 30 ){
+            $errors .= '<div class="alert alert-danger">Le prenom doit contenir entre 2 et 30 caractères. </div>';
+        }
+    
+        if(!isset($_POST['prenom']) || strlen($_POST['prenom']) < 2 || strlen($_POST['nom']) > 30 ){
+            $errors .= '<div class="alert alert-danger">Le prenom doit contenir entre 2 et 30 caractères. </div>';
+        }
+    
+        if(!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL )){
+            $errors .= '<div class="alert alert-danger"> Email non valide. </div>';
+        }
+    
+        if(!isset($_POST['password']) || strlen($_POST['password']) < 8 || strlen($_POST['password']) > 20){
+            $errors .= '<div class="alert alert-danger"> Le mot de passe doit contenir entre 8 et 20 caractères. </div>';
+        }
+
+        if (!preg_match("#[0-9]+#", $_POST['password'])) {
+            $errors .= '<div class="alert alert-danger"> Le mot de passe doit inclure au moins un numéro. </div>';
+        }
+    
+        if (!preg_match("#[a-z]+#", $_POST['password'])) {
+            $errors .= '<div class="alert alert-danger">Le mot de passe doit contenir au moins une lettre. </div>';
+        }  
+
+        if (!preg_match("#[A-Z]+#", $_POST['password'])) {
+            $errors .= '<div class="alert alert-danger">Le mot de passe doit contenir au moins une lettre capitale. </div>';
+        }
+    
+        if(!isset($_POST['password']) || $_POST['password'] != $_POST['password2']){
+            $errors .= '<div class="alert alert-danger">Le mot de passe ne correspond pas.</div>';
+        }
+    
+        if($errors == ""){
+            $showPage = "";
+            unset($_POST['password2']);
+            $_POST['password'] = md5($_POST['password']);//password encrypted
+            if($_GET['op'] == 'newMember'){
+                $newMemberId = $this->db->insert($_POST);
+                if($newMemberId > 0){
+                    $_SESSION['member'] = $this->db->selectOne($newMemberId);
+                    require('views/register_confirm.view.php');
+                }else{
+                    $errors .= '<div class="alert alert-danger" >Votre email est déjà utilisé.</div>';
+                    require('views/sign_up.view.php');
+                }
+            }
+        }else{
+            require('views/sign_up.view.php');
+        }
+    
     }
 }

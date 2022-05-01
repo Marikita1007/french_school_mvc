8f318596627ad75742ac88441d140b6ea76e9108
Marikita1007 <ishinomaki1007@gmail.com>
2022-05-01 11:52:05 +0200

    the edited files

--- views/admin.view.php
+++ views/admin.view.php
@@ -26,7 +26,7 @@ ob_start();
             </nav>
             <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
 
-                <h1 class="h2">Dashboard</h1>
+                <h1 class="h2">Tableau de bord</h1>
                 <p>Voici un tableau de bord pour les utilisateurs de l'administration</p>
 
                 <div class="card">
--- views/edit_question_answer.view.php
+++ views/edit_question_answer.view.php
@@ -24,7 +24,7 @@ ob_start();
                 </div>
             </nav>
             <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
-                <h1 class="h2">Dashboard</h1>
+                <h1 class="h2">Tableau de bord</h1>
                 <p>Voici un tableau de bord pour les utilisateurs de l'administration</p>
 
                 <!-- 問題と解答の入力フォーム -->
